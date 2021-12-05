8ba87fa04b6843ee5eb8c5b220ab2ea0c2eb5622
nagmak <nagmakapoor@gmail.com>
2021-12-05 09:20:42 -0500

    added some form validations

--- components/SignUpSteps/SignUpSteps.tsx
+++ components/SignUpSteps/SignUpSteps.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Step, Steps, useSteps } from 'chakra-ui-steps';
-import { Heading, FormControl, Input, Box, Button, FormLabel, Select, HStack, CircularProgress } from "@chakra-ui/react"
+import { Heading, FormControl, Input, Box, Button, FormLabel, Select, HStack, CircularProgress, Text } from "@chakra-ui/react"
 import router from 'next/router';
 
 import CeramicClient from '@ceramicnetwork/http-client';
@@ -16,7 +16,7 @@ import { TileDocument } from '@ceramicnetwork/stream-tile';
 // we're using a test network here
 const endpoint = "https://ceramic-clay.3boxlabs.com";
 
-const userProfileStep = ({ handleChange }) => {
+const userProfileStep = ({ handleChange, values, errors }) => {
   return (<form style={{ alignSelf: "center"}}>
   <Box h="100%" w="xl" borderWidth="1px" borderRadius="lg" overflow="hidden" cursor="pointer">
       <Box p="4">
@@ -29,34 +29,46 @@ const userProfileStep = ({ handleChange }) => {
                 <HStack spacing={2}>
                   <FormControl p={2} id="first-name" isRequired>
                     <FormLabel>First name</FormLabel>
-                    <Input placeholder="First name" name="firstName" onChange={(event) => handleChange(event)}/>
+                    <Input required isInvalid={errors.firstName} errorBorderColor='red.300' placeholder="First name" name="firstName" value={values.firstName || ''} onChange={handleChange}/>
+                    {errors.firstName && (
+                      <Text fontSize='xs' fontWeight='400' color='red.500'>{errors.firstName}</Text>
+                    )}
                   </FormControl>
                   <FormControl p={2} id="last-name" isRequired>
                     <FormLabel>Last name</FormLabel>
-                    <Input placeholder="Last name" name="lastName" onChange={(event) => handleChange(event)}/>
+                    <Input required isInvalid={errors.lastName} errorBorderColor='red.300' placeholder="Last name" name="lastName" value={values.lastName || ''} onChange={handleChange}/>
+                    {errors.lastName && (
+                      <Text fontSize='xs' fontWeight='400' color='red.500'>{errors.lastName}</Text>
+                    )}
                   </FormControl>
                 </HStack>
                 <FormControl p={2} id="display-name" isRequired>
                   <FormLabel>Display name</FormLabel>
-                  <Input placeholder="Display name" name="displayName" onChange={(event) => handleChange(event)}/>
+                  <Input required isInvalid={errors.displayName} errorBorderColor='red.300' placeholder="Display name" name="displayName" value={values.displayName || ''} onChange={handleChange}/>
+                  {errors.displayName && (
+                      <Text fontSize='xs' fontWeight='400' color='red.500'>{errors.displayName}</Text>
+                    )}
                 </FormControl>
                 <FormControl p={2} id="email" isRequired>
                   <FormLabel>Email</FormLabel>
-                  <Input placeholder="Email" name="email" onChange={(event) => handleChange(event)}/>
+                  <Input required isInvalid={errors.email} errorBorderColor='red.300' placeholder="Email" name="email" value={values.email || ''} onChange={handleChange}/>
+                  {errors.email && (
+                      <Text fontSize='xs' fontWeight='400' color='red.500'>{errors.email}</Text>
+                    )}
                 </FormControl>
                 <HStack spacing={2}>
-                  <FormControl p={2} id="twitter" isRequired>
+                  <FormControl p={2} id="twitter">
                     <FormLabel>Twitter username</FormLabel>
-                    <Input placeholder="Twitter username" name="twitterUsrName" onChange={(event) => handleChange(event)}/>
+                    <Input placeholder="Twitter username" name="twitterUsrName" onChange={handleChange}/>
                   </FormControl>
-                  <FormControl p={2} id="linkedin" isRequired>
+                  <FormControl p={2} id="linkedin">
                     <FormLabel>LinkedIn username</FormLabel>
-                    <Input placeholder="LinkedIn username" name="linkedInUsrName" onChange={(event) => handleChange(event)}/>
+                    <Input placeholder="LinkedIn username" name="linkedInUsrName" onChange={handleChange}/>
                   </FormControl>
                 </HStack>
-                <FormControl p={2} pb={8} id="website" isRequired>
+                <FormControl p={2} pb={8} id="website">
                   <FormLabel>Website</FormLabel>
-                  <Input placeholder="Website" name="website" onChange={(event) => handleChange(event)}/>
+                  <Input placeholder="Website" name="website" onChange={handleChange}/>
                 </FormControl>
         </Box>
     </Box>
@@ -64,7 +76,7 @@ const userProfileStep = ({ handleChange }) => {
 </form>)
 }
 
-const merchantProfileStep = ({ handleChange }) => {
+const merchantProfileStep = ({ handleChange, values, errors }) => {
   return (
     <form style={{ alignSelf: "center"}}>
       <Box h="100%" w="xl" borderWidth="1px" borderRadius="lg" overflow="hidden" cursor="pointer">
@@ -77,23 +89,32 @@ const merchantProfileStep = ({ handleChange }) => {
               isTruncated>
                       <FormControl p={4} id="business-name" isRequired>
                         <FormLabel>Business legal name</FormLabel>
-                        <Input placeholder="Business legal name" name="businessName" onChange={(event) => handleChange(event)}/>
+                        <Input required isInvalid={errors.businessName} errorBorderColor='red.300' value={values.businessName || ''} placeholder="Business legal name" name="businessName" onChange={handleChange}/>
+                        {errors.businessName && (
+                          <Text fontSize='xs' fontWeight='400' color='red.500'>{errors.businessName}</Text>
+                        )}
                       </FormControl>
                       <FormControl p={4} id="business-type" isRequired>
                         <FormLabel>Business type</FormLabel>
-                        <Select placeholder="Select business type" name="businessType" onChange={(event) => handleChange(event)}>
+                        <Select placeholder="Select business type" name="businessType" onChange={handleChange}>
                           <option>Sole proprietorship</option>
                           <option>Partnership</option>
                           <option>Corporation</option>
                         </Select>
+                        {/* {errors.businessType && (
+                          <Text fontSize='xs' fontWeight='400' color='red.500'>{errors.businessType}</Text>
+                        )} */}
                     </FormControl>
                     <FormControl p={4} id="business-location" isRequired>
                         <FormLabel>Business location</FormLabel>
-                        <Select placeholder="Select business location" name="businessLocation" onChange={(event) => handleChange(event)}>
+                        <Select placeholder="Select business location" name="businessLocation" onChange={handleChange}>
                           <option>United States</option>
                           <option>Canada</option>
                           <option>India</option>
                         </Select>
+                        {/* {errors.businessLocation && (
+                          <Text fontSize='xs' fontWeight='400' color='red.500'>{errors.businessType}</Text>
+                        )} */}
                     </FormControl>
             </Box>
         </Box>
@@ -102,7 +123,7 @@ const merchantProfileStep = ({ handleChange }) => {
 )};
   
 
-const professionalProfileStep = ({ handleChange }) => {
+const professionalProfileStep = ({ handleChange, values, errors }) => {
   return (
     <form style={{ alignSelf: "center"}}>
       <Box h="100%" w="xl" borderWidth="1px" borderRadius="lg" overflow="hidden" cursor="pointer">
@@ -115,11 +136,14 @@ const professionalProfileStep = ({ handleChange }) => {
               isTruncated>
                       <FormControl p={4} id="current-company-title" isRequired>
                         <FormLabel>Current company title</FormLabel>
-                        <Input placeholder="Current company title" name="currCompanyTitle" onChange={(event) => handleChange(event)}/>
+                        <Input isInvalid={errors.currCompanyTitle} errorBorderColor='red.300' value={values.currCompanyTitle || ''} required placeholder="Current company title" name="currCompanyTitle" onChange={handleChange}/>
+                        {errors.currCompanyTitle && (
+                          <Text fontSize='xs' fontWeight='400' color='red.500'>{errors.currCompanyTitle}</Text>
+                        )}
                       </FormControl>
                       <FormControl p={4} id="current-location">
                         <FormLabel>Current location</FormLabel>
-                        <Select placeholder="Select current location" name="currLocation" onChange={(event) => handleChange(event)}>
+                        <Select placeholder="Select current location" name="currLocation" onChange={handleChange}>
                           <option>United States</option>
                           <option>Canada</option>
                           <option>India</option>
@@ -127,20 +151,26 @@ const professionalProfileStep = ({ handleChange }) => {
                     </FormControl>
                     <FormControl p={4} id="functional-expertise" isRequired>
                         <FormLabel>Functional expertise</FormLabel>
-                        <Select placeholder="Select functional expertise" name="funcExpertise" onChange={(event) => handleChange(event)}>
+                        <Select placeholder="Select functional expertise" name="funcExpertise" onChange={handleChange}>
                           <option>Marketing</option>
                           <option>Accounting</option>
                           <option>Software Development</option>
                           <option>Creative</option>
                         </Select>
+                        {/* {errors.funcExpertise && (
+                          <Text fontSize='xs' fontWeight='400' color='red.500'>{errors.funcExpertise}</Text>
+                        )} */}
                     </FormControl>
                     <FormControl p={4} id="industry-expertise" isRequired>
                         <FormLabel>Industry expertise</FormLabel>
-                        <Select placeholder="Select industry expertise" name="industryExpertise" onChange={(event) => handleChange(event)}>
+                        <Select placeholder="Select industry expertise" name="industryExpertise" onChange={handleChange}>
                           <option>VC</option>
                           <option>Financial Services</option>
                           <option>Healthcare</option>
                         </Select>
+                        {/* {errors.industryExpertise && (
+                          <Text fontSize='xs' fontWeight='400' color='red.500'>{errors.industryExpertise}</Text>
+                        )} */}
                     </FormControl>
             </Box>
         </Box>
@@ -150,8 +180,11 @@ const professionalProfileStep = ({ handleChange }) => {
 
 const SignUpSteps = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
+  // const [isContinueDisabled, setIsContinueDisabled] = useState(false);
   const [isAccTypeSelection, setIsAccTypeSelection] = useState(true);
   const [isAccTypeSelected, setIsAccTypeSelected] = useState(false);
+  const [values, setValues] = useState({});
+  const [errors, setErrors] = useState({});
   const [accType, setAccType] = useState('');
   const [btnActive, setBtnActive] = useState(0);
   const [identity, setIdentity] = useState({
@@ -160,20 +193,85 @@ const SignUpSteps = () => {
     email: "",
   })
 
+  const validate = (values) => {
+    let errors: any = {};
+    console.log(values);
+    if (!values.firstName) {
+      errors.firstName = 'First name is required';
+    }
+
+    if (!values.lastName) {
+      errors.lastName = 'Last name is required';
+    }
+
+    if (!values.displayName) {
+      errors.displayName = 'Display name is required';
+    }
+  
+    if (!values.email) {
+      errors.email = 'Email address is required';
+    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+      errors.email = 'Email address is invalid';
+    }
+
+    if (!values.businessName) {
+      errors.businessName = 'Business name is required';
+    }
+
+    if (!values.businessType) {
+      errors.businessType = 'Business type is required';
+    }
+
+    // if (!values.businessLocation) {
+    //   errors.businessType = 'Business location is required';
+    // }
+
+    if (!values.currCompanyTitle) {
+      errors.currCompanyTitle = 'Current company is required';
+    }
+
+    // if (!values.funcExpertise) {
+    //   errors.funcExpertise = 'Functional expertise is required';
+    // }
+
+    // if (!values.industryExpertise) {
+    //   errors.industryExpertise = 'Industry expertise is required';
+    // }
+  
+    return errors;
+  };
+
   const handleChange = (evt) => {
+    evt.persist();
+    setValues(values => ({ ...values, [evt.target.name]: evt.target.value }));
+    setErrors(validate(values));
+    console.log(errors);
     const value = evt.target.value;
     setIdentity({
       ...identity,
       [evt.target.name]: value
     });
-
+    console.log(values);
     console.log(identity);
   }
 
+  const handleSubmit = () => {
+    setErrors(validate(values));
+    console.log(errors);
+  }
+
+  // useEffect(() => {
+  //   if (Object.keys(errors).length === 0) {
+  //     console.log("CONTINUE FORTH");
+  //     // handleSubmit();
+  //     // setIsContinueDisabled(false);
+  //   }
+  // }, [errors]);
+
   const steps = [
-    { label: 'User Profile', content: userProfileStep({ handleChange })},
-    { label: 'Merchant Profile', content: merchantProfileStep({ handleChange })},
-    { label: 'Professional Profile', content: professionalProfileStep({ handleChange })},
+    { label: 'User Profile', content: userProfileStep({ handleChange, values, errors })},
+    { label: 'Merchant Profile', content: merchantProfileStep({ handleChange, values, errors })},
+    { label: 'Professional Profile', content: professionalProfileStep({ handleChange, values, errors })},
   ];
 
     // Get user's eth address
@@ -206,9 +304,9 @@ const SignUpSteps = () => {
       const ceramic = new CeramicClient(endpoint);      
       const threeIdConnect = new ThreeIdConnect();
       const provider = new EthereumAuthProvider(window.ethereum, address);
-  
+
       setIsSubmitted(true);
-  
+
       await threeIdConnect.connect(provider);
   
       const did = new DID({
@@ -342,6 +440,7 @@ const SignUpSteps = () => {
             {activeStep < 0 ? router.push('/') :
             <Button pl={264} alignSelf="left" onClick={() => {
                 prevStep();
+                // setIsContinueDisabled(true);
               }} colorScheme="gray" variant="link">
               Back
             </Button>}
@@ -350,9 +449,11 @@ const SignUpSteps = () => {
         ))}
       </Steps>
       <Button w="md" alignSelf="center" onClick={() => {
-          nextStep();
+          // setIsContinueDisabled(true);
           if (activeStep > 2) {
             updateAccType();
+          } else {
+            nextStep();
           }
         }} colorScheme="teal">
         Continue {isSubmitted ? <CircularProgress size="22px" thickness="4px" isIndeterminate color="#3C2E26" /> : null}
