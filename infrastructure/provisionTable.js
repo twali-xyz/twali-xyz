1e7614a8318c2126ee1461275fada4b5f17e43bc
nagmak <nagmakapoor@gmail.com>
2022-02-17 15:41:45 -0500

    local testing

--- infrastructure/provisionTable.js
+++ infrastructure/provisionTable.js
@@ -12,7 +12,7 @@ let dynamodb = new AWS.DynamoDB();
 
 // Table Definition to deploy locally and to production
 let params = {
-    TableName: "user_profile_test",
+    TableName: "nagma_profile_test",
     KeySchema: 
     [
         { AttributeName: "user_name", KeyType: "HASH"},        // Partition Key 
@@ -39,24 +39,24 @@ let params = {
     // Only a replicated instance for production. Not Supported by local.
     if (!process.env.LOCAL_DYNAMO_DB_ENDPOINT) {
 
-        AWS.config.update({ region: "us-east-2"});
+        AWS.config.update({ region: "us-east-1"});
 
         dynamodb = new AWS.DynamoDB();
 
         await dynamodb.createTable(params).promise();
 
-        console.log("Created table in us-east-2");
+        console.log("Created table in us-east-1");
 
 
         const createGlobalTableParams = {
-            GlobalTableName: "user_profile_test",
+            GlobalTableName: "nagma_profile_test",
             ReplicationGroup: [
                 {
                     RegionName: "us-east-1",
                 },
-                {
-                    RegionName: "us-east-2"
-                }
+                // {
+                //     RegionName: "us-east-2"
+                // }
             ]
         };
 
