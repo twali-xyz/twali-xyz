ee7a5aa1b763ca6eb99fa4c1e0dbc285e69a0827
nagmak <nagmakapoor@gmail.com>
2022-03-02 20:10:41 -0500

    env variable vercel

--- pages/[username].tsx
+++ pages/[username].tsx
@@ -3,7 +3,7 @@ import HeaderNav from "../components/HeaderNav/HeaderNav";
 import ProfileDetails from "../components/Profile/ProfileDetails";
 
 export const getStaticPaths = async () => {
-  let apiURL = process.env.NEXT_PUBLIC_LOCALHOST ? process.env.NEXT_PUBLIC_LOCALHOST : `https://${process.env.NEXT_PUBLIC_URL}`; // TEMP: vercel URL doesn't include http
+  let apiURL = process.env.NEXT_PUBLIC_LOCALHOST ? process.env.NEXT_PUBLIC_LOCALHOST : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`; // TEMP: vercel URL doesn't include http
   const res = await fetch(`${apiURL}/api/users/getUsers`);
 
   // Should get a list of all users from the backend here
@@ -24,7 +24,7 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async (context) => {
-  let apiURL = process.env.NEXT_PUBLIC_LOCALHOST ? process.env.NEXT_PUBLIC_LOCALHOST : `https://${process.env.NEXT_PUBLIC_URL}`; // TEMP: vercel URL doesn't include http
+  let apiURL = process.env.NEXT_PUBLIC_LOCALHOST ? process.env.NEXT_PUBLIC_LOCALHOST : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`; // TEMP: vercel URL doesn't include http
   const userName = context.params.userName;
   console.log("Context:", context);
 
