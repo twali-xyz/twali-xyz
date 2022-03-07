8ab88ccbbe9a6eee675d174af9da83d1534253f0
hone1er <24376928+hone1er@users.noreply.github.com>
2022-03-07 14:10:00 +0100

    adjust knowledge and expert logo sizes

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,7 +104,7 @@ const IndexPage = () => {
                 alt="twali knowledge symbol"
                 top={["2px", "3px", "4px", "5px"]}
                 left={["2px", "3px", "4px", "5px"]}
-                height={["52px"]}
+                height={["42px", "52px"]}
                 pos={"relative"}
                 width={"30.4px"}
                 marginX={[
@@ -155,8 +155,8 @@ const IndexPage = () => {
                     alt="twali expert symbol"
                     mx={["8px", "12px", "16px", "20px"]}
                     top={["2px", "3px", "4px"]}
-                    width={["64px"]}
-                    height={["48px"]}
+                    width={["48px", "64px"]}
+                    height={["36px", "48px"]}
                     pos={"relative"}
                   />{" "}
                 </Text>
