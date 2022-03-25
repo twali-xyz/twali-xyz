e1346a8186fd725171ce84618479fc8ae0f51642
hone1er <24376928+hone1er@users.noreply.github.com>
2022-03-25 11:37:45 +0100

    fix react key error

--- components/Profile/EditProfileModal/EditProfileModal.tsx
+++ components/Profile/EditProfileModal/EditProfileModal.tsx
@@ -246,30 +246,10 @@ const EditProfileModal = (props) => {
                   )}
               </FormControl>
               <FormControl p={2} id="current-company-title" isRequired>
-                <FormLabel
-                  marginBottom={1}
-                  pos={"relative"}
-                  fontFamily={"PP Telegraf"}
-                  fontSize={"16px"}
-                  fontStyle={"normal"}
-                  fontWeight={"400"}
-                  lineHeight={"24p"}
-                  letterSpacing={"0.02em"}
-                  textAlign={"left"}
-                >
-                  Current title
-                </FormLabel>
+                <FormLabel>Current title</FormLabel>
                 <Input
-                  px={2}
-                  fontSize="16px"
-                  borderColor={"#587070"}
-                  height={"40px"}
-                  borderRadius={"4px"}
-                  marginBottom={"12px"}
                   isInvalid={errors.currTitle}
                   errorBorderColor="red.300"
-                  fontFamily={"PP Telegraf light"}
-                  _placeholder={{ color: "#98B2B2" }}
                   defaultValue={values.currTitle || ""}
                   required
                   placeholder="Current title"
