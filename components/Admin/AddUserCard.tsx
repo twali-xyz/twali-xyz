297703c5992a06a2814ae452ac655727f61c8176
hone1er <24376928+hone1er@users.noreply.github.com>
2022-06-09 11:09:03 -0700

    add placeholders and last name field

--- components/Admin/AddUserCard.tsx
+++ components/Admin/AddUserCard.tsx
@@ -86,12 +86,13 @@ export const AddUserCard = ({ onClose, ...props }) => {
     <Box
       {...props}
       width={"100%"}
-      height={"fit-content"}
-      background={"n6"}
-      padding={"32px"}
       border={"1px"}
-      borderRadius={"8px"}
+      padding={"32px"}
+      background={"n6"}
       borderColor={"n3"}
+      borderRadius={"8px"}
+      mb={"8px !important"}
+      height={"fit-content"}
       boxShadow="8px 8px 24px 0px #00000026"
       _hover={{ boxShadow: "8px 8px 32px 8px #00000026" }}
     >
@@ -101,53 +102,91 @@ export const AddUserCard = ({ onClose, ...props }) => {
         alignItems={"flex-start"}
       >
         <HStack width={"100%"} justifyContent={"space-between"}>
-          <VStack alignItems={"baseline"}>
+          <VStack alignItems={"baseline"} maxW={"475px"}>
             <FormControl>
               <HStack>
-                <FormLabel width={"100px"}>Name:</FormLabel>
+                <FormLabel width={"70px"} marginRight={"0 !important"}>
+                  Name:
+                </FormLabel>
                 <Input
-                  width={"80%"}
+                  width={"39%"}
                   name="firstName"
+                  _placeholder={{ color: "n3" }}
+                  placeholder={"first"}
+                  onChange={(e) => handleChange(e)}
+                />
+                <Input
+                  width={"39%"}
+                  name="lastName"
+                  _placeholder={{ color: "n3" }}
+                  placeholder={"last"}
                   onChange={(e) => handleChange(e)}
                 />
               </HStack>
             </FormControl>
             <FormControl>
               <HStack>
-                <FormLabel width={"100px"}>Email:</FormLabel>
+                <FormLabel width={"70px"} marginRight={"0 !important"}>
+                  Email:
+                </FormLabel>
                 <Input
                   width={"80%"}
                   name="email"
+                  _placeholder={{ color: "n3" }}
+                  placeholder={"example@twali.xyz"}
                   onChange={(e) => handleChange(e)}
                   type={"email"}
                 />
               </HStack>
             </FormControl>
-            <HStack>
-              <FormLabel width={"100px"}>Discord:</FormLabel>
-              <Input
-                width={"80%"}
-                name="discord"
-                onChange={(e) => handleChange(e)}
-              />
-            </HStack>
             <FormControl>
               <HStack>
-                <FormLabel width={"100px"}>linkedIn:</FormLabel>
+                <FormLabel width={"70px"} marginRight={"0 !important"}>
+                  Discord:
+                </FormLabel>
+                <Input
+                  width={"80%"}
+                  name="discord"
+                  _placeholder={{ color: "n3" }}
+                  placeholder={"username"}
+                  onChange={(e) => handleChange(e)}
+                />
+              </HStack>
+            </FormControl>
+            <FormControl>
+              <HStack>
+                <FormLabel width={"70px"} marginRight={"0 !important"}>
+                  linkedIn:
+                </FormLabel>
                 <Input
                   width={"80%"}
                   name="linkedIn"
+                  isTruncated
+                  _placeholder={{ color: "n3" }}
+                  placeholder={
+                    "https://www.linkedin.com/in/cokie-hasiotis-9b666363/"
+                  }
                   onChange={(e) => handleChange(e)}
                 />
               </HStack>
             </FormControl>
           </VStack>
-          <VStack alignSelf={"flex-start"} alignItems={"flex-start"}>
+          <VStack
+            alignSelf={"flex-start"}
+            alignItems={"flex-start"}
+            minW={"475px"}
+          >
             <FormControl>
               <HStack>
-                <FormLabel width={"100px"}>Status:</FormLabel>
-                <Select width={"80%"}>
-                  <option>select one</option>
+                <FormLabel width={"70px"} marginRight={"0 !important"}>
+                  Status:
+                </FormLabel>
+                <Select
+                  name="status"
+                  width={"80%"}
+                  color={values.status ? "fresh" : "n3"}
+                  placeholder={"select one"}
+                >
                   <option value={"completed"}>completed</option>
                   <option value={"approved"}>approved</option>
                   <option value={"rejected"}>rejected</option>
@@ -157,10 +196,15 @@ export const AddUserCard = ({ onClose, ...props }) => {
 
             <FormControl>
               <HStack>
-                <FormLabel width={"100px"}>Wallet:</FormLabel>
+                <FormLabel width={"70px"} marginRight={"0 !important"}>
+                  Wallet:
+                </FormLabel>
                 <Input
                   width={"80%"}
                   name="userWallet"
+                  isTruncated
+                  placeholder={"0x54eb82e4ec25eb173e1668dd5ab0943904d87331"}
+                  _placeholder={{ color: "n3" }}
                   onChange={(e) => handleChange(e)}
                 />
               </HStack>
